4b4821bf1c2319b6098133501eb8ad9bc8097cb6
Renan Nascimento <84254369+Renanqn@users.noreply.github.com>
2024-09-21 09:14:19 -0300

    Teste Bandit e Dependecy Check - SECRET KEY RESOLVIDAS 1

--- todo_project/todo_project/__init__.py
+++ todo_project/todo_project/__init__.py
@@ -1,21 +1,38 @@
+from dotenv import load_dotenv
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 
+# Carrega as variáveis do arquivo .env
+load_dotenv()
+
+# Define o caminho do diretório base
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+# Inicializa a aplicação Flask
 app = Flask(__name__)
+
+# Configura o banco de dados usando SQLite
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'site.db')
-app.config['SECRET_KEY'] = '45cf93c4d41348cd9980674ade9a7356'
+
+# Carrega o SECRET_KEY da variável de ambiente, lança um erro se não estiver definida
+secret_key = os.getenv('SECRET_KEY')
+if not secret_key:
+    raise ValueError("SECRET_KEY is not set in environment variables.")
+app.config['SECRET_KEY'] = secret_key
+
+# Inicializa o SQLAlchemy com a aplicação
 db = SQLAlchemy(app)
 
+# Configura o Login Manager
 login_manager = LoginManager(app)
-login_manager.login_view = 'login' 
+login_manager.login_view = 'login'
 login_manager.login_message_category = 'danger'
 
+# Inicializa o Bcrypt para hashing de senhas
 bcrypt = Bcrypt(app)
 
-# Always put Routes at end
+# Importa as rotas no final para evitar problemas de importação circular
 from todo_project import routes
